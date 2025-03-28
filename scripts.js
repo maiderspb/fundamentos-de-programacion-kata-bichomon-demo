449d422b60dfcac3d2d7d76cde8f0d9f770aa2cb
Maider San Pedro <maiderspb90@yahoo.es>
2025-03-28 20:31:51 +0100

    JAVASCRIPT - DOM manipulation -Bichomon

--- scripts.js
+++ scripts.js
@@ -1,1 +1,15 @@
 console.log(document.title);
+document.querySelector("#gen-1").textContent = "Generasión 1 Pokimon";
+document.querySelector(".infocard-list").style.backgroundColor = "#FFD700";
+console.log(document.URL);
+console.log(document.domain);
+const imagenes = document.querySelectorAll("img");
+console.log("Lista de imágenes:", imagenes);
+imagenes.forEach(
+  (img) => (img.src = "https://media.giphy.com/media/2v170e71aanfi/giphy.gif")
+);
+document.querySelectorAll(".infocard-lg-data.text-muted").forEach((card) => {
+  if (card.innerText.includes("Flying")) {
+    card.style.backgroundColor = "#87CEEB";
+  }
+});
